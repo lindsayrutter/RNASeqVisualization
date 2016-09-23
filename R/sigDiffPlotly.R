fe7ac8cfb1ee0f0b36c0f2b861081ec2275db316
Lindsay Rutter <lindsayannerutter@gmail.com>
2016-09-22 19:23:01 -0500

    marker list size

--- R/sigDiffPlotly.R
+++ R/sigDiffPlotly.R
@@ -75,7 +75,7 @@ plot_ly(data = iris, x = Sepal.Length, y = Petal.Length, mode = "markers")
 
 plot_ly(data = dat, x = x, y = y, mode = "markers", color = x, colors = "Set1")
 
-plot_ly(data = dat, x = x, y = y, mode = "markers", color = x, colors = "Set1")
+plot_ly(data = dat, x = x, y = y, mode = "markers", marker = list(size = 12), color = x, colors = "Set1")
 
 geom_point(aes(colour = factor(x)), shape = 20, size=5)
 

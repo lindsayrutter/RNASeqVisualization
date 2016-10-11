9e2b8554342a15dc3919b8d3ab5590d50c0516f5
Lindsay Rutter <lindsayannerutter@gmail.com>
2016-10-11 12:17:06 -0500

    changed mode from "markers" to "scatter" and add_trace, add_segment due to plotly changes

--- R/sigDiffPlotly.R
+++ R/sigDiffPlotly.R
@@ -1,10 +1,12 @@
-library(nullabor)
+# This will interactively plot the 100 most significant DEGs in plotly.
+
+library(nullabor) #
 library(rtracklayer)
 library(Rsamtools)
 library(grid)
 library(GenomicAlignments)
 library(ggplot2)
-library(GGally)
+library(GGally) #
 library(edgeR)
 library(stringr)
 library(EDASeq)
@@ -15,60 +17,37 @@ library(reshape2)
 library(scales)
 library(tidyr)
 library(gtools)
+library(plotly)
 
 rm(list=ls())
 load("data/All_wasp.rda")
 
-permList <- list()
-permInfo <- list()
-
 listcond = rep(c("DR","DU"),each= 6)
-permInfo[[1]] = listcond
-
-for(i in 1:3){
-  if (i>1){
-    permInfo[[i]] = permute(listcond)
-  }
-
-  y = DGEList(counts=countTable[,c(1:12)], group=permInfo[[i]])
-
-  keep <- rowSums(cpm(y)>1) >= 6
-  y <- y[keep, keep.lib.sizes=FALSE]
-
-  y <- calcNormFactors(y)
-  y = estimateCommonDisp(y)
-  y = estimateTagwiseDisp(y)
-
-  de = exactTest(y, pair=c("DR","DU"))
-  tt = topTags(de, n=nrow(y))
-
-  nc = cpm(y, normalized.lib.sizes=TRUE)
-  rn = rownames(tt$table)
-
-  permList[[i]] = cbind(nc[rn,order(permInfo[[i]])], tt$table)
-}
-
-for (i in 1:100){
-    gene = permList[[1]][i,1:12]
-    rep = 6
-    fact = 2
-    dat = data.frame(x=rep(1:fact, each=rep),y=t(gene),z=rep(1:rep, times = fact))
-    colnames(dat)=c("x","y","rep")
-    dat$x=as.factor(dat$x)
-    levels(dat$x)=c("DR","DU")
-    genePlot = ggplot(dat, aes(x, y)) + geom_point(aes(colour = factor(x)), shape = 20, size=5) + scale_shape(solid = FALSE) + ylab("Read Count") + ggtitle(paste("Transcript:", rownames(gene), " FDR: ", permList[[1]][i,]$FDR,3)) + scale_y_continuous(limits=c(0, max(dat$y))) + theme(axis.title.x = element_blank(), legend.position="bottom", axis.text=element_text(size=12), axis.title=element_text(size=12), legend.title=element_text(size=12), legend.text=element_text(size=12)) + labs(colour = "Group", size=12) + geom_segment(aes(x = 1, y = mean(dat$y[1:6]), xend = 2, yend = mean(dat$y[7:12])))
-
-    jpeg(file = paste(getwd(), "/Perm1/", "Gene_", i, ".jpg", sep=""), height = 700, width = 700)
-    print(genePlot)
-    dev.off()
-}
-
-
-
-
-###########################################################################
+y = DGEList(counts=countTable[,c(1:12)], group=listcond)
+keep <- rowSums(cpm(y)>1) >= 6
+y <- y[keep, keep.lib.sizes=FALSE]
+y <- calcNormFactors(y)
+y = estimateCommonDisp(y)
+y = estimateTagwiseDisp(y)
+de = exactTest(y, pair=c("DR","DU"))
+tt = topTags(de, n=nrow(y))
+nc = cpm(y, normalized.lib.sizes=TRUE)
+rn = rownames(tt$table)
+geneList = cbind(nc[rn,order(listcond)], tt$table)
 
 ax <- list(title = "", showticklabels = TRUE)
 ay <- list(title = "Read Count")
 
-dat %>% plot_ly(x = x, y = y, mode = "markers", marker = list(size = 12), color = x, colors = "Set1", hoverinfo = "text", text = paste("Read count = ", format(round(dat$y, 2), nsmall = 2))) %>% layout(title = paste("Transcript =", rownames(gene), "<br> FDR =", formatC(permList[[1]][i,]$FDR, format = "e", digits = 2)), xaxis = ax, yaxis = ay, legend = list(x = 0.35, y = -0.26)) %>% add_trace(x = c("DU", "DR"), y= c(mean(dat$y[7:12]),mean(dat$y[1:6])), mode = "lines", showlegend = FALSE, hoverinfo = "text", text = paste(c("(DU)", "(DR)"), "Mean read count =", format(round(c(mean(dat$y[7:12]),mean(dat$y[1:6])), 2), nsmall = 2)))
+for (i in 1:100){
+  gene = geneList[i,1:12]
+  rep = 6
+  fact = 2
+  dat = data.frame(x=rep(1:fact, each=rep),y=t(gene),z=rep(1:rep, times = fact))
+  colnames(dat)=c("x","y","rep")
+  dat$x=as.factor(dat$x)
+  levels(dat$x)=c("DR","DU")
+
+  # changed from mode = "markers" to type = "scatter"
+  # changed 1 add_trace to 2 add_trace and 1 add_segments (x and y were problem variables)
+  dat %>% plot_ly(x = ~x, y = ~y, type = "scatter", marker = list(size = 10), color = ~x, colors = "Set2", hoverinfo = "text", text = paste0("Read count = ", format(round(dat$y, 2), nsmall = 2))) %>% layout(title = paste("Transcript =", rownames(gene), "<br> FDR =", formatC(geneList[i,]$FDR, format = "e", digits = 2)), xaxis = ax, yaxis = ay, legend = list(x = 0.35, y = -0.26)) %>% add_segments(x = "DU", xend = "DR", y = mean(filter(dat, x=="DU")$y), yend = mean(filter(dat, x=="DR")$y), showlegend = FALSE, line = list(color='#000000')) %>% add_trace(x = "DR", y= mean(filter(dat, x=="DR")$y), showlegend = FALSE, hoverinfo = "text", text = paste0("Mean Read Count = ", round(mean(filter(dat, x=="DR")$y), digits = 2)), marker = list(color='#000000')) %>% add_trace(x = "DU", y= mean(filter(dat, x=="DU")$y), showlegend = FALSE, hoverinfo = "text", text = paste0("Mean Read Count = ", round(mean(filter(dat, x=="DU")$y), digits = 2)), marker = list(color='#000000'))
+}

34c09e9983d74eb9ee7d025cc826115081d354d7
Lindsay Rutter <lindsayannerutter@gmail.com>
2016-10-26 23:07:07 -0500

    20 permutation plot for DU/DR

--- R/sigDiffPermLineup.R
+++ R/sigDiffPermLineup.R
@@ -27,23 +27,25 @@ library(tibble)
 rm(list=ls())
 load("data/All_wasp.rda")
 
+nPerm <- 20
+nRep <- 6
 permList <- list()
 permInfo <- list()
 correctPlace <- list()
 
-listcond = rep(c("DR", "DU"), each = 6)
+listcond = rep(c("DR", "DU"), each = nRep)
 permInfo[[1]] = listcond
 
 if (!dir.exists(paste(getwd(), "/PermLineup", sep=""))){
   dir.create(paste(getwd(), "/PermLineup", sep=""))
 }
 
-for(i in 1:20){
+for(i in 1:nPerm){
     if (i>1){
     # Check that permutations include equal number of treatments in each group (check that permutations are thoroughly shuffled)
     while (length(permInfo) < i){
       myPerm = permute(listcond)
-      if (all(table(myPerm[1:6]) == table(myPerm[7:12]))){
+      if (all(table(myPerm[1:nRep]) == table(myPerm[(nRep+1):(2*nRep)]))){
         if (!all(myPerm == permInfo[[i-1]])){
           permInfo[[i]] = myPerm
         }
@@ -51,9 +53,9 @@ for(i in 1:20){
     }
   }
 
-  y = DGEList(counts=countTable[,c(1:12)], group=permInfo[[i]])
+  y = DGEList(counts=countTable[,c(1:(2*nRep))], group=permInfo[[i]])
 
-  keep <- rowSums(cpm(y)>1) >= 6
+  keep <- rowSums(cpm(y)>1) >= nRep
   y <- y[keep, keep.lib.sizes=FALSE]
 
   y <- calcNormFactors(y)
@@ -72,11 +74,11 @@ for(i in 1:20){
 
 for (i in 1:100){
   fullDat <- data.frame()
-  lineup <- permute(seq(1:3))
+  lineup <- permute(seq(1:nPerm))
   correctPlace[i] <- which(lineup==1)
-  for (j in 1:20){
-    gene = permList[[j]][i,1:12]
-    rep = 6
+  for (j in 1:nPerm){
+    gene = permList[[j]][i,1:(2*nRep)]
+    rep = nRep
     fact = 2
     dat = data.frame(x=rep(1:fact, each=rep),y=t(gene),z=which(lineup==j))
     colnames(dat)=c("x","y","z")
@@ -86,9 +88,9 @@ for (i in 1:100){
     dat$meanDU = mean(filter(dat, x=="DU")$y)
     fullDat <- rbind(fullDat, dat)
   }
-    allPlot = ggplot(fullDat, aes(x, y)) + geom_point(aes(colour = factor(x)), shape = 20, size=5, alpha = 0.5) + scale_shape(solid = FALSE) + ggtitle(paste("Transcript: ", i)) + ylab("Read Count") + scale_y_continuous(limits=c(0, max(fullDat$y))) + theme(axis.title.x = element_blank(), legend.position="bottom", axis.text=element_text(size=12), axis.title=element_text(size=12), legend.title=element_text(size=12), legend.text=element_text(size=12), plot.title=element_text(hjust=0.5)) + labs(colour = "Group", size=12) + geom_segment(aes(x = 1, y = meanDR, xend = 2, yend = meanDU), colour="gray25", size = 0.1) + facet_grid(. ~ z)
+    allPlot = ggplot(fullDat, aes(x, y)) + geom_point(aes(colour = factor(x)), shape = 20, size=5, alpha = 0.5) + scale_shape(solid = FALSE) + ggtitle(paste("Transcript: ", i)) + ylab("Read Count") + scale_y_continuous(limits=c(0, max(fullDat$y))) + theme(axis.title.x = element_blank(), legend.position="bottom", axis.text=element_text(size=12), axis.title=element_text(size=12), legend.title=element_text(size=12), legend.text=element_text(size=12), plot.title=element_text(hjust=0.5)) + labs(colour = "Group", size=12) + geom_segment(aes(x = 1, y = meanDR, xend = 2, yend = meanDU), colour="gray25", size = 0.1) + facet_wrap(~ z, ncol = 5)
 
-    jpeg(file = paste(getwd(), "/PermLineup/Gene", i, ".jpg", sep=""), height = 700, width = 700)
+    jpeg(file = paste(getwd(), "/PermLineup/Gene", i, ".jpg", sep=""), height = ceiling(nPerm/5)*175, width = 700)
     print(allPlot)
     dev.off()
 }
@@ -96,12 +98,11 @@ for (i in 1:100){
 correctPlace = data.frame(correctPlace)
 colnames(correctPlace) = 1:100
 correctPlace = t(correctPlace)
-colnames(correctPlace) = "CorrectPlot"
+colnames(correctPlace) = "PlotWithData"
 correctPlace = data.frame(correctPlace)
 correctPlace <- rownames_to_column(correctPlace, "Gene")
 write.csv(correctPlace, row.names = FALSE, file = paste(getwd(), "/PermLineup/Correct.csv", sep=""))
 
-permInfo <- data.frame(t(data.frame(permInfo[[1]], permInfo[[2]], permInfo[[3]])))
-rownames(permInfo) <- c("Real data", "Permutation 1", "Permutation 2")
-write.csv(permInfo, col.names = FALSE, file = paste(getwd(), "/PermLineup/Permutations.csv", sep=""))
+permInfo <- data.frame(matrix(unlist(permInfo), nrow = nPerm))
+rownames(permInfo) <- c("Data", paste0(rep("Permute",(nPerm-1)),1:(nPerm-1)))
 write.table(permInfo, sep=",",  col.names=FALSE, file = paste(getwd(), "/PermLineup/Permutations.csv", sep=""))

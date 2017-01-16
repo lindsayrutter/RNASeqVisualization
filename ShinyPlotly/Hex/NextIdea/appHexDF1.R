2ebfa9bf82a53499a617d4174fe6909937e0200b
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-01-16 11:19:23 -0600

    curveNumber to ID

--- ShinyPlotly/Hex/NextIdea/appHexDF1.R
+++ ShinyPlotly/Hex/NextIdea/appHexDF1.R
@@ -1,10 +1,9 @@
-# Taking into account most recent suggestions - but only with one plot (no facets)
-
 library(shiny)
 library(plotly)
 library(data.table)
 library(GGally)
-library (reshape2)
+library(reshape2)
+library(hexbin)
 
 ui <- fluidPage(
   plotlyOutput("plot"),
@@ -12,31 +11,21 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
-
-  set.seed(1) #Create data
+  #Create data
+  set.seed(1)
   bindata <- data.frame(x=rnorm(100), y=rnorm(100))
-  # fac_probs <- dnorm(seq(-3, 3, length.out=26))
-  # fac_probs <- fac_probs/sum(fac_probs)
-  # bindata$factor <- sample(letters, 100, replace=TRUE, prob=fac_probs)
-  # bindata$factor <- as.factor (bindata$factor)
 
   h <- hexbin (bindata, xbins = 5, IDs = TRUE, xbnds = range (bindata$x), ybnds = range (bindata$y))
 
-  #counts <- hexTapply (h, bindata$factor, table)
-  #counts <- t (simplify2array (counts))
-  #counts <- melt (counts)
-  # Each of the 20 facets has 26 IDs (with values between 3 and 40) (20*26=520)
-  # Counts between 0 and 5
-  #colnames (counts)  <- c ("ID", "factor", "counts")
-
   # As we have the cell IDs, we can merge this data.frame with the proper coordinates
   hexdf <- data.frame (hcell2xy (h),  ID = h@cell, counts = h@count)
-  #hexdf <- merge (counts, hexdf)
-  # ggplotting (use the command below) this yields the correct bin sizes, but the figure has a bit weird appearance: 0 count hexagons are drawn, but only where some other facet has this bin populated.
-  #hexdf$counts [hexdf$counts == 0] <- NA
-  #hexdf$counts <- h@count
 
-  p <- ggplot(hexdf, aes(x=x, y=y, fill = counts)) + geom_hex(stat="identity")
+  # I have tried different methods of generating the ggplot object
+  #p <- ggplot(hexdf, aes(x=x, y=y, fill = counts)) + geom_hex(stat="identity")
+  #p <- ggplot(hexdf, aes(x=x, y=y, fill = ID)) + geom_hex(stat="identity")
+  #p <- ggplot(hexdf, aes(x=x, y=y, fill = counts, colours = ID)) + geom_hex(stat="identity")
+  #p <- ggplot(hexdf, colours = ID, aes(x=x, y=y, colours = ID, fill = counts)) + geom_hex(stat="identity")
+  p <- ggplot(hexdf, aes(x=x, y=y, fill = counts, ID=ID)) + geom_hex(stat="identity")
 
   output$plot <- renderPlotly({
     ggplotly(p)
@@ -49,10 +38,9 @@ server <- function(input, output, session) {
       "Click on a state to view event data"
     }
     else{
-      #str(d())
-      #d()$curveNumber
-      hexdf[d()$curveNumber,]
-      #hexdf[d()$curveNumber,]$ID
+      str(d())
+      #Next line would deliver all observations from original data frame (bindata) that are in the clicked hexbin... if d() from event_data() was returning ID instead of curveNumber
+      #bindata[which(h@cID==d()$curveNumber),]
     }
   })
 }

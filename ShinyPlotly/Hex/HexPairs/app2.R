ac966d52e5b0aaa7c7ff39b39fce73b856808ba2
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-01-18 15:51:11 -0600

    scatmat working using hexbin (instead of geom_hex)

--- ShinyPlotly/Hex/HexPairs/app2.R
+++ ShinyPlotly/Hex/HexPairs/app2.R
@@ -29,54 +29,21 @@ server <- function(input, output, session) {
 
   bindata <- data.frame(ID = paste0("ID",1:100), A=rnorm(100), B=rnorm(100), C=rnorm(100), D=rnorm(100), E=rnorm(100))
   bindata$ID <- as.character(bindata$ID)
-  h <- hexbin (bindata[,-1], xbins = 5, IDs = TRUE, xbnds = range (bindata$A), ybnds = range (bindata$B))
-  hexdf <- data.frame (hcell2xy (h),  hexID = h@cell, counts = h@count)
-  p <- ggplot(hexdf, aes(x=x, y=y, fill = counts, hexID=hexID)) + geom_hex(stat="identity")
-  cnID <- cnToID(h)
-
 
   my_fn <- function(data, mapping, ...){
-    # data is 100 rows of
-    #A           B C D E
-    #1 -0.3410670 -0.70756823 1 1 1
-    #2  1.5024245  1.97157201 1 1 1
-    #3  0.5283077 -0.08999868 1 1 1
-    #str(data)
-    # data[,c(varA)] ; varA = "A"
-    #mappingX <- as.name("A")
-    #is.name(an) # TRUE
-    #mode(an)   # name
-    #typeof(an) # symbol
-    xbnds = range (data[,c(as.character(mapping$x))])
-    ybnds = range (data[,c(as.character(mapping$y))])
-
-    #h <- hexbin (data, xbins = 5, IDs = TRUE, xbnds = range (data[,c(as.character(mapping$x))]), ybnds = range (data[,c(as.character(mapping$y))]))
-
-    #if(!missing(xbnds) && any(sign(xbnds - range(x)) == c(1,-1)))
-
-    print(str(mapping$x))
-    print(str(mapping$y))
-    print(xbnds)
-    print(ybnds)
-    print(range(x))
-    print(range(y))
-    #print(head(data))
-    #print(str(h))
-
-    #hexdf <- data.frame (hcell2xy (h),  hexID = h@cell, counts = h@count)
-    #p <- ggplot(hexdf, aes(x=mapping$x, y=mapping$y, fill = counts, hexID=hexID)) + geom_hex(stat="identity")
+    x = data[,c(as.character(mapping$x))]
+    y = data[,c(as.character(mapping$y))]
+    h <- hexbin(x=x, y=y, xbins=5, IDs=TRUE, xbnds=range(x), ybnds=range(y))
+    hexdf <- data.frame (hcell2xy (h),  hexID = h@cell, counts = h@count)
+    p <- ggplot(hexdf, aes(x=x, y=y, fill = counts, hexID=hexID)) + geom_hex(stat="identity")
     #cnID <- cnToID(h)
-    #p
+    p
   }
-
-
-
-  if(!missing(xbnds) && any(sign(xbnds - range(x)) == c(1,-1)))
 
   # To 7 (include counts column, but not hexID)
   p <- ggpairs(bindata[,2:6], lower = list(continuous = my_fn))
-  myMax = max(data[,2:6])
-  myMin = min(data[,2:6])
+  myMax = max(bindata[,2:6])
+  myMin = min(bindata[,2:6])
   pS <- p
   for(i in 2:p$nrow) {
     for(j in 1:(i-1)) {

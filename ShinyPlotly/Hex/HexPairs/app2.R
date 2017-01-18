6583e443e0cc2918904fb44da5135d0697e5ab2a
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-01-18 16:38:23 -0600

    scatMat of hexBin equal sizes

--- ShinyPlotly/Hex/HexPairs/app2.R
+++ ShinyPlotly/Hex/HexPairs/app2.R
@@ -30,10 +30,14 @@ server <- function(input, output, session) {
   bindata <- data.frame(ID = paste0("ID",1:100), A=rnorm(100), B=rnorm(100), C=rnorm(100), D=rnorm(100), E=rnorm(100))
   bindata$ID <- as.character(bindata$ID)
 
+  maxVal = max(abs(bindata[,2:6]))
+  maxRange = c(-1*maxVal, maxVal)
+
   my_fn <- function(data, mapping, ...){
     x = data[,c(as.character(mapping$x))]
     y = data[,c(as.character(mapping$y))]
-    h <- hexbin(x=x, y=y, xbins=5, IDs=TRUE, xbnds=range(x), ybnds=range(y))
+    #print(maxRange)
+    h <- hexbin(x=x, y=y, xbins=5, shape=1, IDs=TRUE, xbnds=maxRange, ybnds=maxRange)
     hexdf <- data.frame (hcell2xy (h),  hexID = h@cell, counts = h@count)
     p <- ggplot(hexdf, aes(x=x, y=y, fill = counts, hexID=hexID)) + geom_hex(stat="identity")
     #cnID <- cnToID(h)
@@ -42,14 +46,12 @@ server <- function(input, output, session) {
 
   # To 7 (include counts column, but not hexID)
   p <- ggpairs(bindata[,2:6], lower = list(continuous = my_fn))
-  myMax = max(bindata[,2:6])
-  myMin = min(bindata[,2:6])
   pS <- p
   for(i in 2:p$nrow) {
     for(j in 1:(i-1)) {
       pS[i,j] <- p[i,j] +
-        scale_x_continuous(limits = c(myMin, myMax)) +
-        scale_y_continuous(limits = c(myMin, myMax))
+        scale_x_continuous(limits = maxRange) +
+        scale_y_continuous(limits = maxRange)
     }
   }
 
@@ -65,9 +67,6 @@ server <- function(input, output, session) {
     }
     else{
       str(d())
-      d()$curveNumber
-      data[d()$curveNumber,]
-      data[d()$curveNumber,]$ID
     }
   })
 }

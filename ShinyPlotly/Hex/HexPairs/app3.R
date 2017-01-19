458b106d57e6de702c2bf68930fcc6784b153948
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-01-18 22:06:01 -0600

    Discrepancy in visualizaing hexBins happens after scale_x/y_continuous

---
+++ ShinyPlotly/Hex/HexPairs/app3.R
@@ -0,0 +1,70 @@
+library(shiny)
+library(plotly)
+library(data.table)
+library(GGally)
+library(hexbin)
+
+ui <- fluidPage(
+  plotlyOutput("plot"),
+  verbatimTextOutput("click")
+)
+
+server <- function(input, output, session) {
+
+  # Curve number to ID
+  cnToID <- function(h){
+    df <- data.frame(table(h@cID))
+    colnames(df) <- c("hexID","count")
+    cnID <- df[order(df$count,as.character(df$hexID)),]
+    cnID$curveNumber <- seq(0, nrow(cnID)-1)
+    return(cnID)
+  }
+
+  set.seed(1)
+  bindata <- data.frame(ID = paste0("ID",1:100), A=rnorm(100), B=rnorm(100), C=rnorm(100), D=rnorm(100), E=rnorm(100))
+  bindata$ID <- as.character(bindata$ID)
+
+  maxVal = max(abs(bindata[,2:6]))
+  maxRange = c(-1*maxVal, maxVal)
+
+  my_fn <- function(data, mapping, ...){
+    x = data[,c(as.character(mapping$x))]
+    y = data[,c(as.character(mapping$y))]
+    print(as.character(mapping$x))
+    print(as.character(mapping$y))
+    h <- hexbin(x=x, y=y, xbins=5, shape=1, IDs=TRUE, xbnds=maxRange, ybnds=maxRange)
+    hexdf <- data.frame (hcell2xy (h),  hexID = h@cell, counts = h@count)
+    print(str(hexdf))
+    p <- ggplot(hexdf, aes(x=x, y=y, fill = counts, hexID=hexID)) + geom_hex(stat="identity")
+    #cnID <- cnToID(h)
+    p
+  }
+
+  # To 7 (include counts column, but not hexID)
+  p <- ggpairs(bindata[,2:6], lower = list(continuous = my_fn))
+  pS <- p
+  for(i in 2:p$nrow) {
+    for(j in 1:(i-1)) {
+      pS[i,j] <- p[i,j] +
+        scale_x_continuous(limits = maxRange) +
+        scale_y_continuous(limits = maxRange)
+    }
+  }
+
+  output$plot <- renderPlotly({
+    ggplotly(pS)
+  })
+
+  d <- reactive(event_data("plotly_click"))
+
+  output$click <- renderPrint({
+    if (is.null(d())){
+      "Click on a state to view event data"
+    }
+    else{
+      str(d())
+    }
+  })
+}
+
+shinyApp(ui, server)

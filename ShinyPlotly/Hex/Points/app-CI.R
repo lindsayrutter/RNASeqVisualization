02425c5811f88f0eee277712a101384d22daba7c
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-01-27 10:34:55 -0600

    app-Scale-NoPlotly and app-Scale-Plotly

--- ShinyPlotly/Hex/Points/app-CI.R
+++ ShinyPlotly/Hex/Points/app-CI.R
@@ -11,14 +11,13 @@ keep <- abs(x - y) >= ciVal
 
 data <- data.frame(dp = paste0("DataPoint",1:sum(keep==TRUE)), x = x[keep], y = y[keep])
 data$dp <- as.character(data$dp)
-data2 <- data.frame(x = seq(myMin, myMax, 0.01), y = seq(myMin, myMax, 0.01))
+data2 <- data.frame(x = c(myMin, myMax), y = c(myMin, myMax))
 
 ggplot(data = data, aes(x=x,y=y)) +
   geom_point(size=2) +
   geom_ribbon(data=data2, aes(x=x, ymin = y-ciVal, ymax = y+ciVal), fill = "lightgrey") +
   geom_abline(intercept = 0, color = "white", size = 0.25) +
-  #geom_abline(intercept = ciVal, color ="blue", size = 0.25) +
-  #geom_abline(intercept = -1*ciVal, color ="blue", size = 0.25) +
   scale_x_continuous(limits = c(myMin, myMax)) +
   scale_y_continuous(limits = c(myMin, myMax)) +
-  scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0))
+  scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0)) +
+  theme_bw()

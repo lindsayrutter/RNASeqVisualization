45ececd7af34589617eb06d83b7ab60ea8b416b1
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-02-21 20:27:28 -0600

    Final s.o.

--- plotlyHexPoint/soGGHexPlot.R
+++ plotlyHexPoint/soGGHexPlot.R
@@ -2,14 +2,7 @@ library(plotly)
 library(data.table)
 library(GGally)
 library(hexbin)
-
-cnToID <- function(h){
-  df <- data.frame(table(h))
-  colnames(df) <- c("hexID","count")
-  cnID <- df[order(df$count,as.character(df$hexID)),]
-  cnID$curveNumber <- seq(1, nrow(cnID))
-  return(cnID)
-}
+library(htmlwidgets)
 
 set.seed(1)
 bindata <- data.frame(ID = paste0("ID",1:100), A=rnorm(100), B=rnorm(100))
@@ -17,11 +10,10 @@ bindata$ID <- as.character(bindata$ID)
 
 x = bindata[,c("A")]
 y = bindata[,c("B")]
-h <- hexbin(x=x, y=y, xbins=5, shape=1, IDs=TRUE, xbnds=maxRange, ybnds=maxRange)
+h <- hexbin(x=x, y=y, xbins=5, shape=1, IDs=TRUE)
 hexdf <- data.frame (hcell2xy (h),  hexID = h@cell, counts = h@count)
 attr(hexdf, "cID") <- h@cID
-pS <- ggplot(hexdf, aes(x=x, y=y, fill = counts, hexID=hexID)) + geom_hex(stat="identity"
-)
+pS <- ggplot(hexdf, aes(x=x, y=y, fill = counts, hexID=hexID)) + geom_hex(stat="identity")
 
 ggPS <- ggplotly(pS)
 
@@ -36,16 +28,14 @@ for (i in 1:myLength){
 
 ggPS %>% onRender("
           function(el, x, data) {
-            console.log(el)
-            console.log(x)
-            console.log(data)
+            //console.log(el)
+            //console.log(x)
+            //console.log(data)
 
             myGraph = document.getElementById(el.id);
             el.on('plotly_click', function(e) {
 
             cN = e.points[0].curveNumber
-            console.log(cN)
-            console.log(x.data[cN].yaxis)
             split1 = (x.data[cN].text).split(' ')
             hexID = (x.data[cN].text).split(' ')[2]
             counts = split1[1].split('<')[0]
@@ -55,6 +45,7 @@ ggPS %>% onRender("
            })}
            ", data = pS$data)
 
-hexID=20
+# I know how to access values from outside onRender
+hexID=40
 obsns <- which(attr(pS$data, "cID")==hexID)
 dat <- bindata[obsns,]

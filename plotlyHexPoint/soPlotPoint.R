ef3fee71722910fe414e244730174b0b983c542f
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-02-25 13:25:41 -0600

    Plotting multiple values on single scatterplot efficiently

--- plotlyHexPoint/soPlotPoint.R
+++ plotlyHexPoint/soPlotPoint.R
@@ -3,28 +3,32 @@ library(htmlwidgets)
 
 g <- ggplot(mtcars, aes(x=mpg, y=cyl)) + geom_point()
 gP <- ggplotly(g)
+gP %>%
+  layout(
+    xaxis = list(range = c(8, 35)),
+    yaxis = list(range = c(3, 9))
+  )
 
 gP %>% onRender("
-          function(el, x) {
-            myGraph = document.getElementById(el.id);
+function(el, x) {
+  myGraph = document.getElementById(el.id);
+  el.on('plotly_click', function(e)
+    {
 
-            el.on('plotly_click', function(e) {
-
-              console.log(e)
-              console.log(e.points[0].x)
-
-              var trace1 = {
-                x: [e.points[0].x-.3, e.points[0].x-.3, e.points[0].x+.3, e.points[0].x+.3],
-                y: [e.points[0].y-.3, e.points[0].y+.3, e.points[0].y-.3, e.points[0].y+.3],
-                type: 'scatter',
-                fillColor : 'red',
-                size: 20
-              };
-
-              Plotly.addTraces(el.id, trace1);
-           })}
-           ")
-
-#Plotly.addTraces(el.id, trace1);
-# restyle
-# redraw
+      var trace1 = {
+        x: [20, 25, 30],
+        y: [5, 5, 5],
+        mode: 'markers',
+        marker: {
+          color: 'red',
+          size: 20
+        }
+      };
+      //for (var i = 0; i < 100; i+= 1){
+      //  trace1.x.push(e.points[0].x + Math.random())
+      //  trace1.y.push(e.points[0].y + Math.random())
+      //}
+    Plotly.addTraces(el.id, trace1);
+  }
+)}
+")

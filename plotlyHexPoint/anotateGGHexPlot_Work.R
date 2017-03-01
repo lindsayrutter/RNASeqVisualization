1baf14653141b071c04950a9ccd3edb90f0e6ea9
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-03-01 12:39:28 -0600

    Work on redrawing CI on single plot with onRender()

--- plotlyHexPoint/anotateGGHexPlot_Work.R
+++ plotlyHexPoint/anotateGGHexPlot_Work.R
@@ -7,7 +7,6 @@ library(htmlwidgets)
 set.seed(1)
 bindata <- data.frame(ID = paste0("ID",1:100), A=rnorm(100), B=rnorm(100), C=rnorm(100), D=rnorm(100), E=rnorm(100))
 bindata$ID <- as.character(bindata$ID)
-colNames = colnames(bindata[-1])
 
 maxVal = max(abs(bindata[,2:6]))
 maxRange = c(-1*maxVal, maxVal)
@@ -18,7 +17,7 @@ my_fn <- function(data, mapping, ...){
   h <- hexbin(x=x, y=y, xbins=5, shape=1, IDs=TRUE, xbnds=maxRange, ybnds=maxRange)
   hexdf <- data.frame (hcell2xy (h),  hexID = h@cell, counts = h@count)
   attr(hexdf, "cID") <- h@cID
-  p <- ggplot(hexdf, aes(x=x, y=y, fill = counts, hexID=hexID)) + geom_hex(stat="identity")
+  p <- ggplot(hexdf, aes(x=x, y=y, fill = counts, hexID=hexID)) + geom_hex(stat="identity") + geom_abline(intercept = 0, color = "red", size = 0.25)
   p
 }
 

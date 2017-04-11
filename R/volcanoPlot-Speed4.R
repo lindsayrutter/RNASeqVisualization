e43666852617d2ed78525580d4e641d00ca09a8a
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-04-11 11:48:15 -0500

    isolated FC and pval, but can't access datInput

--- R/volcanoPlot-Speed4.R
+++ R/volcanoPlot-Speed4.R
@@ -5,13 +5,13 @@ ui <- shinyUI(pageWithSidebar(
   headerPanel("Click the button"),
   sidebarPanel(
     sliderInput("threshP", "P-value:", min = 0, max = 1, value=1, step=0.05),
-    sliderInput("threshFC", "Fold change:", min = 0, max = 10, value=0, step=0.5),
-
+    #sliderInput("threshFC", "Fold change:", min = 0, max = 10, value=0, step=0.5),
+    uiOutput("slider"),
     actionButton("goButton", "Go!")
   ),
   mainPanel(
     plotlyOutput("plot1"),
-    verbatimTextOutput("selectedValues")
+    verbatimTextOutput("click")
   )
 ))
 
@@ -29,6 +29,12 @@ server <- shinyServer(function(input, output) {
   xMin = min(dat$FC)
   yMax = max(dat$pval)
   yMin = min(dat$pval)
+  maxTemp = max(abs(xMax), abs(xMin))
+
+  #make dynamic slider
+  output$slider <- renderUI({
+    sliderInput("threshFC", "Fold change:", min=0, max=ceiling(maxTemp), value=ceiling((maxTemp)/3), step=0.5)
+  })
 
   output$plot1 <- renderPlotly({
     # Take a dependency on input$goButton
@@ -36,9 +42,22 @@ server <- shinyServer(function(input, output) {
 
     # Use isolate() to avoid dependency on input$obs
     datInput <- isolate(subset(dat , pval < input$threshP & input$threshFC < FC))
-    p <- qplot(datInput$FC, datInput$pval)
+    p <- qplot(datInput$FC, datInput$pval) +xlim(0, ceiling(maxTemp)) +ylim(0,1)
     ggplotly(p)
   })
+
+  d <- reactive(event_data("plotly_selected"))
+
+  output$click <- renderPrint({
+    if (is.null(d())){
+      "Click on a state to view event data"
+    }
+    else{
+      str(d()$pointNumber)
+      #datInput[d()$pointNumber,]
+    }
+  })
+
 })
 
 shinyApp(ui, server)

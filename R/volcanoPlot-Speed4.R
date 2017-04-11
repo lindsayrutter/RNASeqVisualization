9b79233ec0f526e44b6eb871caf2a0186a6dcfca
Lindsay Rutter <lindsayannerutter@gmail.com>
2017-04-11 14:22:20 -0500

    FC and pval separate and subsetting working

--- R/volcanoPlot-Speed4.R
+++ R/volcanoPlot-Speed4.R
@@ -4,9 +4,8 @@ library(plotly)
 ui <- shinyUI(pageWithSidebar(
   headerPanel("Click the button"),
   sidebarPanel(
-    sliderInput("threshP", "P-value:", min = 0, max = 1, value=1, step=0.05),
-    #sliderInput("threshFC", "Fold change:", min = 0, max = 10, value=0, step=0.5),
-    uiOutput("slider"),
+    sliderInput("val1", "Value 1:", min = 0, max = 1, value=0.5, step=0.1),
+    sliderInput("val2", "Value 2:", min = 0, max = 1, value=0.5, step=0.1),
     actionButton("goButton", "Go!")
   ),
   mainPanel(
@@ -18,33 +17,23 @@ ui <- shinyUI(pageWithSidebar(
 server <- shinyServer(function(input, output) {
 
   set.seed(1)
-  #threshP <- reactive(input$threshP)
-  #threshFC <- reactive(input$threshFC)
+  dat <- data.frame(Case = paste0("case",1:15), val1=runif(15,0,1), val2=runif(15,0,1))
+  dat$Case <- as.character(dat$Case)
 
-  dat <- data.frame(ID = paste0("ID",1:14), FC=runif(14,0,10), pval=runif(14,0,1))
-  dat$ID <- as.character(dat$ID)
-
-  # x-axis FC, y-axis pval
-  xMax = max(dat$FC)
-  xMin = min(dat$FC)
-  yMax = max(dat$pval)
-  yMin = min(dat$pval)
+  xMax = max(dat$val1)
+  xMin = min(dat$val1)
+  yMax = max(dat$val2)
+  yMin = min(dat$val2)
   maxTemp = max(abs(xMax), abs(xMin))
 
-  #make dynamic slider
-  output$slider <- renderUI({
-    sliderInput("threshFC", "Fold change:", min=0, max=ceiling(maxTemp), value=ceiling((maxTemp)/3), step=0.5)
-  })
-
-  output$plot1 <- renderPlotly({
-    # Take a dependency on input$goButton
-    input$goButton
-
-    # Use isolate() to avoid dependency on input$obs
-    datInput <- isolate(subset(dat , pval < input$threshP & input$threshFC < FC))
-    p <- qplot(datInput$FC, datInput$pval) +xlim(0, ceiling(maxTemp)) +ylim(0,1)
-    ggplotly(p)
-  })
+  observeEvent(input$goButton,
+       output$plot1 <- renderPlotly({
+         # Use isolate() to avoid dependency on input$val1 and input$val2
+         datInput <- isolate(subset(dat, val1 > input$val1 & val2 > input$val2))
+         p <- qplot(datInput$val1, datInput$val2) +xlim(0, ceiling(maxTemp)) +ylim(0,1)
+         ggplotly(p)
+       })
+  )
 
   d <- reactive(event_data("plotly_selected"))
 
@@ -53,11 +42,10 @@ server <- shinyServer(function(input, output) {
       "Click on a state to view event data"
     }
     else{
-      str(d()$pointNumber)
-      #datInput[d()$pointNumber,]
+      #str(d()$pointNumber)
+      datInput[d()$pointNumber,] #Error: object 'datInput' not found
     }
   })
-
 })
 
 shinyApp(ui, server)
